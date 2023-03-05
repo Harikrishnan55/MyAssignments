e4a466dce88cd0f63346307d86bdd53d2ccdc94a
Harikrishnan55 <127012368+Harikrishnan55@users.noreply.github.com>
2023-03-05 14:19:49 +0530

    Add files via upload
    Day1

---
+++ day1/VehicleDetails.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class VehicleDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int nofWheels = 4;
+        boolean isnew = true;
+            char logo = 'V';
+     String brandname = "Volkswagen";
+         float milage = 18.9f;
+         long enginenumber = 19870505;
+         double kms = 3000.3;
+         
+         System.out.println(" No of Wheels : " + nofWheels );
+         System.out.println(" Car Logo : " + logo );
+         System.out.println(" Brand Name : " + brandname );
+         System.out.println(" Milage : " + milage);
+         System.out.println(" Enginenumber :" + enginenumber);
+         System.out.println(" Total Driven KMs :" + kms);
+	}
+
+}
